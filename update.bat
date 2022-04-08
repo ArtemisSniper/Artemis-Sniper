e7d554dbcf7d4346988b79d036c66f7007354f42
Smint <smintoverflow@gmail.com>
2022-04-08 16:49:43 +0530

    [Chore] Update `update.bat`

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
-title Artemis-Updater
+title Artemis Updater
 curl "https://raw.githubusercontent.com/Everest187/Artemis-Sniper/main/artemis.py" --output artemis.py --silent
-echo Successfully Updated Artemis
+echo Artemis has been successfully updated!
 pause
