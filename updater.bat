80af9781350ee613c48a21c90dadaaec6be8042d
Everest187 <99863675+Everest187@users.noreply.github.com>
2022-04-07 22:36:16 -0400

    Batch file for updating Artemis-Sniper (Windows)

---
+++ updater.bat
@@ -0,0 +1,5 @@
+@echo off
+title Artemis-Updater
+curl "https://raw.githubusercontent.com/Everest187/Artemis-Sniper/main/artemis.py" --output artemis.py --silent
+echo Successfully Updated Artemis
+pause
